72b2665b43c64894f3351e65a4b2618e5619fc22
Nathan Loo <supermario9927@gmail.com>
2021-04-15 13:43:51 -0500

    Add files via upload

---
+++ TableQuery/candidatedisplay.sql
@@ -0,0 +1,3 @@
+-- this is for showing the table with the attributes we are looking for
+-- there probably could've been a better option of just dropping columns
+SELECT LastName, FirstName, FileDate, PartyName, OfficeName FROM candidates ORDER BY LastName, FirstName;
---
+++ TableQuery/candidates.sql
@@ -0,0 +1,36 @@
+-- this is for pupulating the candidate table in the filing db
+-- after running this query, right click "Tables" in the schema on the left under Filing
+-- click "Table Data Import Wizard" and load the csvs in the github into the candidates table
+create database if not exists Filing;
+use Filing;
+
+DROP TABLE IF EXISTS candidates;
+CREATE TABLE candidates (
+	CandidateID int,
+    BracketID int,
+    SlateID int,
+    LastName varchar(45),
+    FirstName varchar(45),
+    AffiliateCommittee varchar(45),
+    HeadOfSlate varchar(45),
+    Address1 varchar(90),
+    Address2 varchar(90),
+    City varchar(45),
+    State varchar(2),
+    Zip int,
+    FileDate varchar(45),
+    Sequence int,
+    CandidateStatus varchar(45),
+    StatusDate varchar(45),
+    WebsiteAddress varchar(45),
+    ElecDate varchar(45),
+    ElectionType varchar(10),
+    PartyName varchar(45),
+    PartySequence int,
+    OfficeName varchar(45),
+    BallotGroup varchar(45),
+    OfficeSequence int,
+    FormerName varchar(45),
+    EmailAddress varchar(45),
+    primary key (CandidateID)
+);
