ab86ecfd3c1e2628f9e4a526d9a7662b1447deed
Nathan Loo <supermario9927@gmail.com>
2021-04-20 16:17:30 -0500

    Created templates based on the questions that Anya made up

--- TableQuery/templateQueries.sql
+++ TableQuery/templateQueries.sql
@@ -2,53 +2,63 @@ use Filing;
 
 drop procedure if exists searchingparty;
 set delimiter ;;
+-- how many <targetparty> party members ran in general?
 create procedure searchingparty(in targetparty varchar(45))
 begin
 	-- I guess this is one way of storing the result - putting it in a procedure
-    select FirstName, LastName from candidateInfos natural join candidateNames where partyName = targetparty;
+    select FirstName, LastName from candidateInfos
+    natural join candidateNames
+    where PartyName = targetparty;
 end ;;
 set delimiter ;;
 
 drop procedure if exists searchinggender;
 set delimiter ;;
-create procedure searchinggender(in targetpronoun varchar(45))
+-- how many <targetparty> party members ran who use the <targetpronoun> pronouns?
+create procedure searchinggender(in targetparty varchar(45), in targetpronoun varchar(45))
 begin
-	-- I guess this is one way of storing the result - putting it in a procedure
     select FirstName, LastName from candidateRaceGender
     natural join candidateNames
-    where Pronouns = targetpronoun;
+    natural join candidateInfos
+    where Pronouns = targetpronoun
+    and PartyName = targetparty;
 end ;;
 set delimiter ;;
 
 drop procedure if exists searchinghispanic;
 set delimiter ;;
-create procedure searchinghispanic(in targethispanic varchar(45))
+-- how many <targetparty> party members ran who are <targetethnicity> ethnicity?
+create procedure searchinghispanic(in targetparty varchar(45), in targetethnicity varchar(45))
 begin
-	-- I guess this is one way of storing the result - putting it in a procedure
     select FirstName, LastName from candidateRaceGender
     natural join candidateNames
-    where Hispanic = targethispanic;
+    natural join candidateInfos
+    where Hispanic = targetethnicity
+    and PartyName = targetparty;
 end ;;
 set delimiter ;;
 
 drop procedure if exists searchingrace;
 set delimiter ;;
-create procedure searchingrace(in targetrace varchar(45))
+-- how many <targetparty> party members ran who are <targetrace> race?
+create procedure searchingrace(in targetparty varchar(45), in targetrace varchar(45))
 begin
-	-- I guess this is one way of storing the result - putting it in a procedure
     select FirstName, LastName from candidateRaceGender
     natural join candidateNames
-    where Race = targetrace;
+    natural join candidateInfos
+    where Race = targetrace
+    and PartyName = targetparty;
 end ;;
 set delimiter ;;
 
 drop procedure if exists searchingoffice;
 set delimiter ;;
-create procedure searchingoffice(in targetoffice varchar(45))
+-- how many <targetparty> party members ran for <targetoffice> office?
+create procedure searchingoffice(in targetparty varchar(45), in targetoffice varchar(45))
 begin
-	-- I guess this is one way of storing the result - putting it in a procedure
     select FirstName, LastName from candidateInfos
     natural join candidateNames
-    where OfficeName = targetoffice;
+    where OfficeName = targetoffice
+    and PartyName = targetparty;
 end ;;
 set delimiter ;;
